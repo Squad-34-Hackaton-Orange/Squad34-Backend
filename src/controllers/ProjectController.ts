b65d0a59664425ab88f237b12683ec425e459eab
Henry Thuler <henrythuler2003@outlook.com>
2024-01-25 20:52:01 -0300

    feat(controller): creating project controller, getAllUserProjects and createProject routes and changing user schema.

---
+++ src/controllers/ProjectController.ts
@@ -0,0 +1,71 @@
+import { Request, Response } from "express";
+import { prisma, PrismaError } from "../utils/prisma";
+import { UserController } from "./UserController";
+
+export class ProjectController{
+
+    static async CreateProject(req: Request, res: Response){
+
+        const { title, description, link, id_user } = req.body
+
+        if (!title || !description || !link || !id_user) {
+        
+            res.status(400).send({
+                message: "Invalid Data",
+            });
+
+            return;
+
+        }
+
+        try{
+
+            const project = await prisma.project.create({
+                data: {
+                    title,
+                    description,
+                    link,
+                    id_user
+                }
+            })
+
+            res.status(201)
+
+        }catch(error){
+
+            if (error instanceof PrismaError.PrismaClientKnownRequestError) {
+                res.status(500).send(error);
+            }
+        
+            throw error;
+
+        }
+
+    }
+
+    static async GetAllUserProjects(req: Request, res: Response): Promise<void>{
+
+        const {userId} = req.params
+
+        try{
+            
+            const userProjects = await prisma.project.findMany({
+                where: {
+                    id_user: Number(userId)
+                }
+            })
+            res.status(200).json(userProjects)
+
+        }catch(error){
+
+            if (error instanceof PrismaError.PrismaClientKnownRequestError) {
+                res.status(500).send(error);
+            }
+        
+            throw error;
+
+        }
+
+    }
+
+}
---
+++ src/routes/projectRoutes.ts
@@ -0,0 +1,20 @@
+import {Router} from "express";
+import { ProjectController } from "../controllers/ProjectController";
+
+const router = Router();
+
+//CREATE
+router.post('/', ProjectController.CreateProject)
+
+// localhost.com:8080/project/
+
+//READ
+router.get('/user/:userId', ProjectController.GetAllUserProjects);
+
+//UPDATE
+//router.put('/:UserId', UserController.updateUserById)
+
+//DELETE
+//router.delete('/:UserId', UserController.DeleteUserById)
+
+export default router
