8443be2924ce897930d7693b59f1215bfe98bb8c
ErickDs-dev <erycdiogenes@gmail.com>
2024-01-24 22:29:35 -0300

    UserController document updated

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,82 +1,116 @@
-import {Request, Response, NextFunction} from 'express';
-import {PrismaError, prisma} from '../utils/prisma'
-import bcrypt from 'bcrypt'
-
+import { Request, Response, NextFunction } from "express";
+import { PrismaError, prisma } from "../utils/prisma";
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
 
 export class UserController {
-  static async CreateUser(req: Request, res: Response){
-    const {name, last_name, email, password} = req.body;
+  static async CreateUser(req: Request, res: Response) {
+    const { name, last_name, email, password } = req.body;
 
-    
     // TODO: VERIFICAÇÕES
-    if(!name || !last_name || !email || !password){
-
+    if (!name || !last_name || !email || !password) {
       // TODO: TRATAR MENSAGEM
       res.status(400).send({
-        menssage: "Invalid Data"
-      })
+        menssage: "Invalid Data",
+      });
 
       return;
     }
 
-
     const checkUserExist = await prisma.user.findFirst({
-      where:{
-        email
+      where: {
+        email,
       },
       select: {
-        email: true
-      }
-    })
+        email: true,
+      },
+    });
 
-    if(checkUserExist){
+    if (checkUserExist) {
       res.status(409).send({
-        menssage: "User Already Exist."
-      })
+        menssage: "User Already Exist.",
+      });
 
       return;
     }
 
     // HASH DA SENHA
 
-    const hash = bcrypt.hashSync(password, 10)
+    const hash = bcrypt.hashSync(password, 10);
 
-    if(!hash){
+    if (!hash) {
       res.status(500).send({
-        menssage: "Internal Server Error"
-      })      
+        menssage: "Internal Server Error",
+      });
     }
 
     // CRIAÇÃO NO BANCO DE DADOS
 
     try {
+      await prisma.user.create({
+        data: {
+          name,
+          last_name,
+          email,
+          password,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          deletedAt: new Date(),
+        },
+      });
 
-       await prisma.user.create({
-         data:{
-           name,
-           last_name,
-           email,
-           password,
-           createdAt: new Date(),
-           updatedAt: new Date(),
-           deletedAt: new Date(),
-         }
-       })
-
-       res.status(201).send({
-        message: "User Created Sucessfuly."
-       })
-      
-      
-      
+      res.status(201).send({
+        message: "User Created Sucessfuly.",
+      });
     } catch (error) {
-      
-      if( error instanceof PrismaError.PrismaClientKnownRequestError){
-        res.status(500).send(error)
+      if (error instanceof PrismaError.PrismaClientKnownRequestError) {
+        res.status(500).send(error);
       }
 
-      throw error
+      throw error;
     }
   }
 
+  static async updateUserById(req: Request, res: Response): Promise<void> {
+      const {UserId} = req.params;
+       const {
+        name,
+        last_name,
+        email,
+        country} = req.body;
+
+        const getUser = await prisma.user.findUnique({
+          where: { id: Number(UserId) }
+        });
+        
+        if(!getUser) {
+          res.status(404).send({
+            message: "User Notfound.",
+          });
+            return;
+        }
+
+        const updatedUser = {
+          name:name?name:getUser.name,
+          last_name:last_name?last_name:getUser.last_name,
+          email:email?email:getUser.email,
+          country:country?country:getUser.country,
+          updatedAt:new Date()
+        }
+
+    try {
+       await prisma.user.update({
+        where: { id:Number(UserId) },
+        data: updatedUser,
+      });
+
+      res.status(200).send({
+        message: "User Updated Sucessfuly.",
+      });
+    } catch (error) {
+      res.status(500).send({
+        message: "User Updated Error.",
+      });
+    }
+  }
 }
