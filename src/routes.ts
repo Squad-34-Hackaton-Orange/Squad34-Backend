e6416929c2840157993fe1098b650c6e833682e0
Henry Thuler <henrythuler2003@outlook.com>
2024-01-28 13:04:11 -0300

    doc(swagger) - api doc

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import {Express} from 'express';
 import userRoutes from './routes/userRoutes'
 import projectRoutes from './routes/projectRoutes'
+import swaggerUI from 'swagger-ui-express'
+import swaggerDocs from './swagger.json'
 
 export function routes(app:Express){
 
+  app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
   app.use('/user', userRoutes)
   app.use('/project', projectRoutes)
   
