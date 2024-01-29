7b3a0c24264cb6e023b01506e1579301120786e5
Henry Thuler <henrythuler2003@outlook.com>
2024-01-28 21:31:18 -0300

    fix(routes): fixing routes paths and auth id checking.

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -8,15 +8,15 @@ const router = Router();
 router.post('/', authUser, ProjectController.CreateProject)
 
 //READ
-router.get('/?user=:userId', authUser, ProjectController.GetAllUserProjects);
+router.get('/:userId?', authUser, ProjectController.GetAllUserProjects);
 
 // READ ALL
-router.get('/all', authUser, ProjectController.GetAllProjects);
+router.get('/:userId/all', authUser, ProjectController.GetAllProjects);
 
-router.get("/:projectId", authUser, ProjectController.GetProjectById);
+router.get("/:userId/:projectId", authUser, ProjectController.GetProjectById);
 
 //DELETE
-router.delete('/:projectId', authUser, ProjectController.DeleteProject);
+router.delete('/:userId/:projectId', authUser, ProjectController.DeleteProject);
 
 //UPDATE
 router.put('/:projectId', authUser, ProjectController.UpdateProject);
