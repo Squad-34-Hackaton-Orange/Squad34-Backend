e298b54057fd45b9b10bed8c006c064a7c9bf793
Henry Thuler <henrythuler2003@outlook.com>
2024-02-01 20:40:12 -0300

    fix(cors): creating new cors config middleware.

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,16 @@ import express from "express";
 import { routes } from "./routes";
 import dotenv from 'dotenv'
 import cors from 'cors';
+import corsConfig from "./middlewares/corsConfig";
 
 const app = express();
 const port = 8080;
 
 app.use(express.json())
 dotenv.config()
+
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }))
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+app.use(corsConfig)
 
 app.get("/", (req, res) => {
   res.send("Hello World");
