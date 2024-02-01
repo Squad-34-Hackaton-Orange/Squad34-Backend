e1c4250ee7334d2d2f36345501d2297ffebb12d4
Livia <liviahelen025@gmail.com>
2024-01-31 21:54:54 -0300

    Add cors on project.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { routes } from "./routes";
 import dotenv from 'dotenv'
+import cors from 'cors';
 
 const app = express();
 const port = 8080;
 
 app.use(express.json())
 dotenv.config()
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("Hello World");
