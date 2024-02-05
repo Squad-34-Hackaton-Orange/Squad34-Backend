2f487a4a7474329dd206a80017b2228eb1eb5614
xTirian <tirianx@yahoo.com>
2024-02-04 22:19:08 -0300

    WIP: remove

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,17 @@ const origin = process.env.CORS_ORIGIN
 app.use(express.json())
 dotenv.config()
 
-app.use(corsConfig)
-app.use(cors({ credentials: true, origin: origin }))
+//app.use(corsConfig)
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://orange-portfolio-eta.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  // Passa para o próximo middleware
+  next();
+});
+
 
 app.get("/", (req, res) => {
   res.send("Hello World");
